4e05c8e00ff9332b3ab54aea65f0ca126a543703
Petr Tsekoyev <p.tsekoev@gmail.com>
2025-10-10 15:21:21 +0500

    fix: tailwind config + globals import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import Layout from "../components/Layout";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
+import "@/styles/globals.css"; // обязательно!
+
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
